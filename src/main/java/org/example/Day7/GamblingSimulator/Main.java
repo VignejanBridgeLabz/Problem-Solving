5260d04984618dfbfc6cfa4335a480cf9984bdce
VignejanBridgeLabz <vignejan.sivanesan@bridgelabz.com>
2026-01-28 16:30:18 +0530

    commit DAY7 Problems

---
+++ src/main/java/org/example/Day7/GamblingSimulator/Main.java
@@ -0,0 +1,9 @@
+package org.example.Day7.GamblingSimulator;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to GamblingSimulator...");
+        GamplerStimulation gs = new GamplerStimulation();
+        gs.PlayforDays();
+    }
+}
