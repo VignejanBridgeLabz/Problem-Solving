09b5e8385dde5da3819f31b7b6dfa207f38ac5cb
VignejanBridgeLabz <vignejan.sivanesan@bridgelabz.com>
2026-01-29 11:46:03 +0530

    commit the final day 8 problems

--- src/main/java/org/example/Day3/LineComparison/LineCompare.java
+++ src/main/java/org/example/Day3/LineComparison/LineCompare.java
@@ -7,7 +7,7 @@ public class LineCompare {
 
     //find the lenght of the line
     public double lenghtOftheLine(double a, double b, double c,double d) {
-        double lenght= Math.sqrt(Math.pow(a-b,2)+Math.pow(c-d,2));
+        double lenght= Math.sqrt(Math.pow(b-a,2)+Math.pow(d-c,2));
        return lenght;
     }
 }
