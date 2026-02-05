b70b94fad3aa5af80f7a0d362fce2f4be2ea8d33
VignejanBridgeLabz <vignejan.sivanesan@bridgelabz.com>
2026-02-05 12:09:50 +0530

    commit the Day9 Employee Wagew Calculation

---
+++ src/main/java/org/example/Day9/AddressBook/EmployeeWage/Main.java
@@ -0,0 +1,18 @@
+package org.example.Day9.AddressBook.EmployeeWage;
+
+import org.example.Day8.EmployeeWage.EmployeeWage;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to wage Calculator");
+        Employee emp = new Employee();
+        emp.checkAttendance();
+        emp.dailyWageCalculation();
+        emp.employeeMonthlyWageCalculation();
+        emp.wageCalculationByParticularRange();
+
+        EmployeeWage empw = new EmployeeWage();
+        empw.computeEmployeeWage();
+
+    }
+}
