da6843151d0131955c6dbda01ee6da7bb4b063d3
VignejanBridgeLabz <vignejan.sivanesan@bridgelabz.com>
2026-05-17 14:11:26 +0530

    commit the Day 19 Regex problems for email validation

---
+++ src/main/java/org/example/Day19/EmailValidation.java
@@ -0,0 +1,56 @@
+package org.example.Day19;
+
+import java.util.regex.Pattern;
+
+public class EmailValidation {
+    // Email Regex
+    public static final String EMAIL_REGEX =
+            "^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)?@[a-zA-Z0-9]+\\.[a-zA-Z]{2,3}(\\.[a-zA-Z]{2})?$";
+
+    public static boolean validateEmail(String email) {
+        return Pattern.matches(EMAIL_REGEX, email);
+    }
+
+    public static void main(String[] args) {
+
+        // Valid Emails
+        String[] validEmails = {
+                "abc@yahoo.com",
+                "abc-100@yahoo.com",
+                "abc.100@yahoo.com",
+                "abc111@abc.com",
+                "abc-100@abc.net",
+                "abc.100@abc.com.au",
+                "abc@1.com",
+                "abc@gmail.com.com",
+                "abc+100@gmail.com"
+        };
+
+        // Invalid Emails
+        String[] invalidEmails = {
+                "abc",
+                "abc@.com.my",
+                "abc123@gmail.a",
+                "abc123@.com",
+                "abc123@.com.com",
+                ".abc@abc.com",
+                "abc()*@gmail.com",
+                "abc@%*.com",
+                "abc..2002@gmail.com",
+                "abc.@gmail.com",
+                "abc@abc@gmail.com",
+                "abc@gmail.com.1a",
+                "abc@gmail.com.aa.au"
+        };
+
+        System.out.println("===== VALID EMAIL TESTS =====");
+        for (String email : validEmails) {
+            System.out.println(email + " -> " + validateEmail(email));
+        }
+
+        System.out.println("\n===== INVALID EMAIL TESTS =====");
+        for (String email : invalidEmails) {
+            System.out.println(email + " -> " + validateEmail(email));
+        }
+    }
+}
