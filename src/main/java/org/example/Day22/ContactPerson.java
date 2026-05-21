aed51bd9b37326c3d05f66adb04913fb71e8dd88
VignejanBridgeLabz <vignejan.sivanesan@bridgelabz.com>
2026-05-21 15:46:30 +0530

    commit the Day22 Collections

---
+++ src/main/java/org/example/Day22/ContactPerson.java
@@ -0,0 +1,49 @@
+package org.example.Day22;
+
+
+
+public class ContactPerson {
+
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String state;
+    private String zip;
+    private String phoneNumber;
+    private String email;
+
+    public ContactPerson(String firstName, String lastName, String address,
+                         String city, String state, String zip,
+                         String phoneNumber, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    // Getters
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName +
+                " | " + city +
+                " | " + state +
+                " | " + phoneNumber;
+    }
+}
